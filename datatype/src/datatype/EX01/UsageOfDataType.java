373a4c5675c678497c309dd21fef00d4220ca9ef
DaramgeOvO <lovely21144@gmail.com>
2024-05-23 17:15:37 +0900

    commit

---
+++ datatype/src/datatype/EX01/UsageOfDataType.java
@@ -0,0 +1,16 @@
+// 변수 선언과 값을 대입하는 2가지 방법
+package datatype.EX01;
+
+import java.sql.SQLOutput;
+
+public class UsageOfDataType {
+    public static void main(String[] args) {
+        //변수 선언과 함께 값 대입
+        int a = 3;
+        // 변수 선언과 값 대입 분리
+        int b;
+        b = 4;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ datatype/src/datatype/EX03/RangeOfVariableUse.java
@@ -0,0 +1,15 @@
+// 변수의 생존 기간
+package datatype.EX03;
+
+public class RangeOfVariableUse {
+    public static void main(String[] args) {
+        int value1 = 3;
+        {
+            int value2 = 5;
+            System.out.println(value1);
+            System.out.println(value2);
+        }
+        System.out.println(value1);
+        //System.out.println(value2);    오류
+    }
+}
